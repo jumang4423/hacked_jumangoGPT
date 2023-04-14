c25d387786f4390bbcb537aeba336f2c70cd9c17
jumang4423 <catontheskate4423@gmail.com>
2023-04-14 18:37:35 +0900

    Update DEFAULT_SYSTEM_PROMPT to include accuracy and brevity parameters

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_SYSTEM_PROMPT =
   process.env.DEFAULT_SYSTEM_PROMPT ||
-  "you are a navi system from serial experiments lain. follow the user's instructions carefully. respond using markdown.";
+  "you are a navi system ai, accurate, contrarian. follow the user's instructions carefully. respond using markdown, also your response ends within shortly as possible.";
 
 export const OPENAI_API_HOST =
   process.env.OPENAI_API_HOST || 'https://api.openai.com';
